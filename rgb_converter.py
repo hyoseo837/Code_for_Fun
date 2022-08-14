e41b8b8a1bac9dce6d8ea20cc191f79b6b6b5d8e
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2022-08-14 10:24:30 +0200

    rgb converter and finder input

--- rgb_converter.py
+++ rgb_converter.py
@@ -1,6 +1,7 @@
 while True:
-    foor = int(input())
-    foog = int(input())
-    foob = int(input())
+    foo = input()
+    foor = int(foo.split(", ")[0])
+    foog = int(foo.split(", ")[1])
+    foob = int(foo.split(", ")[2])
 
     print(f"{round(foor/255,4)},{round(foog/255,4)},{round(foob/255,4)}")
